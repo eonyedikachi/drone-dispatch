86a9e41943a95f09c162ce157cd694a172ce3910
onyedikachii <onyedikachii@hardcorebiometric.com>
2022-04-02 16:14:13 +0100

    Initial commit

---
+++ src/main/java/com/drone/dispatch/DroneDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.drone.dispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDispatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneDispatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/drone/dispatch/DroneDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drone.dispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDispatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
