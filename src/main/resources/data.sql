63774ab9eeb6d40fc47c2fe1e272bb7cb3028e70
onyedikachii <onyedikachii@hardcorebiometric.com>
2022-04-04 20:25:06 +0100

    Added preloaded data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO DRONE (SERIAL_NUMBER, BATTERY_CAPACITY, MODEL, STATE, WEIGHT_LIMIT)
+VALUES
+    ('350315e9-d90f-43f0-8226-5e5de0cced3f', 40.0, 'Lightweight', 'IDLE', 150.0),
+    ('15cb3a86-2d1d-49ab-94cb-9d1d5be5ba54', 50.0, 'Middleweight', 'IDLE', 300.0),
+    ('edca474c-e8df-4509-9d1d-d8c83736b4a3', 20.0, 'Cruiserweight', 'RETURNING', 350.0),
+    ('0d02ccbb-328e-41a4-830b-c261d51cce2a', 90.0, 'Heavyweight', 'IDLE', 500.0),
+    ('7ee45eb4-e170-4052-8540-fad51e4b8ba7', 65.0, 'Cruiserweight', 'IDLE', 400.0);
+
+INSERT INTO MEDICATION (CODE, NAME, WEIGHT)
+VALUES
+    ('6aa89d53-72dc-4f90-bea5-6c4a7d90f2a0', 'CLARTEM-QS23',  50.0),
+    ('14d7b017-ae4f-4c75-92e4-ac086b9813f4','CLARTEM-TX45', 60.0),
+    ('c1c77b5f-75cc-471e-a502-dd1baf7005a7','CLARTEM-GV70', 100.0),
+    ('691d3a2f-fd9d-4352-a66e-5ff18f5d4f9b','CLARTEM-NX29', 40.0),
+    ('e1bc064f-2809-4856-98f9-c840cb9044f0','CLARTEM-KT88', 300.0);
