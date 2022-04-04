9b4652cf900ffbab251c8eb3549d6d4a74f4917d
onyedikachii <onyedikachii@hardcorebiometric.com>
2022-04-04 10:05:18 +0100

    Added method to get Drone by State value in DroneRepository Interface

--- src/main/java/com/drone/dispatch/repos/DroneRepository.java
+++ src/main/java/com/drone/dispatch/repos/DroneRepository.java
@@ -3,6 +3,8 @@ package com.drone.dispatch.repos;
 import com.drone.dispatch.entities.Drone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
 public interface DroneRepository extends JpaRepository<Drone, String> {
+    List<Drone> findDroneByState(String state);
 }
